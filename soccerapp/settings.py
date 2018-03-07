608538c316c81d4fa7feba4959e5f477d8a3b0ea
IT-corridor <jason.5001001@gmail.com>
2018-03-07 13:15:11 +0530

    #4

--- soccerapp/settings.py
+++ soccerapp/settings.py
@@ -85,9 +85,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'soccerapp.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
