ac2648bbc05b8f033bb873d64c6c0d675a7ca9d7
IT-corridor <jason.5001001@gmail.com>
2018-03-15 06:04:16 +0530

    revise admin

--- general/admin.py
+++ general/admin.py
@@ -15,11 +15,17 @@ class GameInvitationAdmin(admin.ModelAdmin):
     search_fields = ['game', 'player', 'code']
 
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_superuser')
+    search_fields = ['email', 'username', 'first_name', 'last_name']
+    list_filter = ('gender',)
+
+
 class GameEventAdmin(admin.ModelAdmin):
     list_display = ('location', 'datetime', 'created_by', 'description')
 
 
 admin.site.register(Location, LocationAdmin)
-admin.site.register(Player)
+admin.site.register(Player, UserAdmin)
 admin.site.register(GameEvent, GameEventAdmin)
 admin.site.register(GameInvitation, GameInvitationAdmin)
